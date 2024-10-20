29ce3177b91f7722fd9a68a0c7381be2fca5d761
huseyn abdullayev <huseyn.abdullayev.9999@gmail.com>
2024-10-21 00:09:05 +0400

    html

---
+++ script.js
@@ -0,0 +1,88 @@
+1
+// for(i=1;i<100;i++){
+//     console.log(i);
+// }
+
+// let i=1;
+// while(i<100){
+//     console.log(i);
+//     i++;
+
+// }
+
+
+
+2
+// const arr = [5,10,3,5,7];
+// let enkicik = arr[0];
+// for(i=0;i<arr.length;i++){
+
+//     if(arr[i]<enkicik){
+//         enkicik=arr[i];
+//     }
+// }
+// console.log(enkicik);
+
+
+
+3
+// const arr = [5,10,3,5,7];
+// let enboyuk = arr[0];
+// for(i=0;i<arr.length;i++){
+
+//     if(arr[i]>enboyuk){
+//         enboyuk=arr[i];
+//     }
+// }
+// console.log(enboyuk);
+
+
+
+4
+// let eded = 11;
+// let say = 0;
+// for (i = 2; i < eded; i++) {
+//   if (eded % i == 0) {
+//     console.log("eded murekkebdir");
+//     say++;
+//     break;
+//   }
+// }
+// if (say == 0) {
+//   console.log("eded sadedir");
+// }
+
+5
+// let arr = [3, 1, 6, 10, 2, 8, 7]
+// let c = 0;
+// for (i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+
+//         if (arr[j] > arr[j + 1]) {
+//             c = arr[j];
+//             arr[j] = arr[j + 1];
+//             arr[j + 1] = c;
+
+//         }
+//     }
+// }
+
+// console.log(arr);
+
+6
+// let say = 0;
+// let eded = 20;
+// for (i = 2; i < eded; i++) {
+//     for (j = 2; j < i; j++) {
+//         if (i % j == 0) {
+
+
+//             say++;
+//            break;
+//         }
+      
+//     }
+// }
+// console.log(say);
+
+
